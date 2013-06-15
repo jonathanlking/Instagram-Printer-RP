c19c7d840ac52c60b32c17f5e0a55b9f6a0e508e
jonathanlking <jonathanlking@me.com>
2013-06-15 22:53:08 +0200

    Update address.py

--- address.py
+++ address.py
@@ -2,7 +2,13 @@
 
 import subprocess
 
+
 cmd = subprocess.Popen('hciconfig hci0', shell=True, stdout=subprocess.PIPE)
 for line in cmd.stdout:
     if "BD Address" in line:
-        print line
+        data = line.split()
+        for field in data:
+            if field.len() == 'XX:XX:XX:XX:XX:XX'.len():
+                print field
+            
+        
