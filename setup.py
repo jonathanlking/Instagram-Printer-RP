cf1002f647e1ba383a634b7640446605f2580819
jonathanlking <jonathanlking@me.com>
2013-06-15 23:05:04 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@
 import sys
 import os
 import bluetooth
+import subprocess
 
 print "Searching for devices..."
 
@@ -26,7 +27,20 @@ else:
     print "No printer detected, please turn on and run this script again"
     sys.exit()
 
-hostDeviceAddress = '00:10:60:D2:A0:F4'
+hostDeviceAddress = ''
+
+cmd = subprocess.Popen('hciconfig hci0', shell=True, stdout=subprocess.PIPE)
+for line in cmd.stdout:
+    if "BD Address" in line:
+        data = line.split()
+        for field in data:
+            if len(field) == len('XX:XX:XX:XX:XX:XX'):
+                hostDeviceAddress = field
+                
+if len(field) == 0:
+    print "Something went wrong trying to determine the address of you bluetooth adaptor, are you sure it is plugged in and working?"
+    sys.exit()
+
 pathToBluetoothPincodeSettings = '/var/lib/bluetooth/' + hostDeviceAddress
 
 if not os.path.exists(pathToBluetoothPincodeSettings):
