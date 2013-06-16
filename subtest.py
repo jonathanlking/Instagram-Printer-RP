40f221a88480f5e3bcc1c7d759b367fab9e09373
Jonathan King <jonathanlking@me.com>
2013-06-16 22:30:38 +0100

    fix bug

--- subtest.py
+++ subtest.py
@@ -5,7 +5,7 @@ import subprocess
 
 cmd = subprocess.Popen('ussp-push /dev/rfcomm0 abc.jpg file.jpg', shell=True, stderr=subprocess.PIPE)
 output = cmd.communicate()
-print output.rstrip()
+print output
 
 #'sudo rfcomm bind /dev/rfcomm0 00:04:48:1B:87:7F'
 #'ussp-push /dev/rfcomm0 temporaryImage.jpg file.jpg'
