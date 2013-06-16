ead1fa16ca83e5896e1fd65d165c5e539c3607c1
Jonathan King <jonathanlking@me.com>
2013-06-16 16:58:56 +0100

    Added functionality to print.py

--- print.py
+++ print.py
@@ -5,8 +5,7 @@ import os
 import urllib
 import subprocess
 
-photoLink = 'http://instagram.com/p/anrQQ9vA8p/'
-photoLink = ''
+# Get the printer address from 'settings.txt'
 
 try:
     with open('settings.txt','w') as settings:
@@ -14,6 +13,25 @@ try:
 except IOError:
     print "Unable to read printer address, do you have a settings.txt file?"
 
+# Download the image to print from the link
+
+photoLink = 'http://instagram.com/p/anrQQ9vA8p/'
+
 paramaters = urllib.urlencode({'link': photoLink})
 photo = urllib.urlopen("http://instagram.jonathanlking.com/engine/link?%s" % paramaters)
-print photo.read()
+
+# Save the image to file
+
+try:
+    with open('temporaryImage.jpg','w') as file:
+        file.write(photo.read())
+except IOError:
+    print "Unable to write file"
+else:
+    print "Image saved locally"
+
+# Bind the printer to rfcomm0
+
+cmd = subprocess.Popen('sudo rfcomm bind /dev/rfcomm0 ' + printerAddress, shell=True, stdout=subprocess.PIPE)
+for line in cmd.stdout:
+    print line
