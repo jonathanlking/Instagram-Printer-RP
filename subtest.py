011174a198958a60751d4dea68b13f1f86add550
Jonathan King <jonathanlking@me.com>
2013-06-16 22:15:53 +0100

    Update 2

--- subtest.py
+++ subtest.py
@@ -3,5 +3,5 @@ import os
 import bluetooth
 import subprocess
 
-cmd = subprocess.Popen('hciconfig hci0', shell=True, stdout=subprocess.PIPE)
+cmd = subprocess.Popen('ussp-push /dev/rfcomm0 temporaryImage.jpg file.jpg', shell=True, stdout=subprocess.PIPE)
 print cmd.stdout.read()
