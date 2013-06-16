77f00e1336559baee9bd0a0214eb8ad64eedea98
jonathanlking <jonathanlking@me.com>
2013-06-16 17:20:31 +0200

    Update print.sh
    New URL

--- print.sh
+++ print.sh
@@ -5,7 +5,7 @@
 # Takes two arguments, the instagram link of the photo and the address of the printer
 
 # Download the image from the server
-wget http://print.jonathanlking.com/engine/link_jpg?link=$1 -O print.jpg
+wget http://instagram.jonathanlking.com/engine/link?link=$1 -O print.jpg
 
 # Bind to the Pogo Printer
 sudo rfcomm bind /dev/rfcomm0 $2 1
