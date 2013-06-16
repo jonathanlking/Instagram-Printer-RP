c20165a07e62940c5cf6fe5bcd7d95836c6b8b5a
Jonathan King <jonathanlking@me.com>
2013-06-16 22:19:50 +0100

    It is working…!

--- subtest.py
+++ subtest.py
@@ -3,5 +3,11 @@ import os
 import bluetooth
 import subprocess
 
-cmd = subprocess.Popen('ussp-push /dev/rfcomm0 temporaryImage.jpg file.jpg', shell=True, stdout=subprocess.PIPE)
+cmd = subprocess.Popen('sudo rfcomm bind /dev/rfcomm0 00:04:48:1B:87:7F', shell=True, stdout=subprocess.PIPE)
 print cmd.stdout.read()
+print cmd.stdout.read()
+print cmd.stdout.read()
+
+
+#'sudo rfcomm bind /dev/rfcomm0 00:04:48:1B:87:7F'
+#'ussp-push /dev/rfcomm0 temporaryImage.jpg file.jpg'
