db7a8967cd192f1cda20a1cf5b7879613469a2fe
Jonathan King <jonathanlking@me.com>
2013-06-17 19:45:55 +0100

    Now with colours!

--- subtest.py
+++ subtest.py
@@ -3,11 +3,13 @@ import os
 import bluetooth
 import subprocess
 
+from termcolor import colored
+
 cmd = subprocess.Popen('ussp-push /dev/rfcomm0 temporaryImage.jpg file.jpg', shell=True, stderr=subprocess.PIPE)
 output = cmd.communicate()
 
 for line in output:
-	print line
+	print colored(line, 'red')
 	
 #'sudo rfcomm bind /dev/rfcomm0 00:04:48:1B:87:7F'
 #'ussp-push /dev/rfcomm0 temporaryImage.jpg file.jpg'
