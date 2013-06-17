43ad09c9fbc481a89ac51e5824d6fe2f13c0c990
Jonathan King <jonathanlking@me.com>
2013-06-17 19:42:29 +0100

    Test

--- subtest.py
+++ subtest.py
@@ -5,7 +5,9 @@ import subprocess
 
 cmd = subprocess.Popen('sudo rfcomm bind /dev/rfcomm0 00:04:48:1B:87:7F', shell=True, stderr=subprocess.PIPE)
 output = cmd.communicate()
-print output
 
+for line in output:
+	print line
+	
 #'sudo rfcomm bind /dev/rfcomm0 00:04:48:1B:87:7F'
 #'ussp-push /dev/rfcomm0 temporaryImage.jpg file.jpg'
