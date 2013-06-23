ec05e1c9e6da89d4b3fa597131138fa068467745
Jonathan King <jonathanlking@me.com>
2013-06-23 13:05:26 +0100

    Updated code

--- photo.py
+++ photo.py
@@ -1,4 +1,17 @@
+import sys
+import os
 import urllib
+import subprocess
+import bluetooth
+
+TEMP_FILE_NAME = 'temporaryImage.jpg'
+
+def downloadPhotoFromLink(link):
+	
+	paramaters = urllib.urlencode({'link': link})
+	photo = urllib.urlopen("http://instagram.jonathanlking.com/engine/link?%s" % paramaters)
+	return photo
+
 
 def savePhotoWithName(photo, name):
 
@@ -11,4 +24,20 @@ def savePhotoWithName(photo, name):
 	else:
 		print "Image saved locally"
 		
-savePhotoWithName(photo, "tempTest")
+# Get the image link
+
+imageLink = ''
+
+if len(sys.argv) > 1:
+    imageLink = sys.argv[1]
+else:
+    print 'No link provided'
+    sys.exit()
+
+# Download the image to print from the link
+
+photo = downloadPhotoFromLink(imageLink)
+
+# Save the image to file
+
+savePhotoWithName(photo, TEMP_FILE_NAME)
